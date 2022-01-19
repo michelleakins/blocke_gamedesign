7c7b2f2b325911ff7635d86775ed8d52be848b4e
michelleakins <michellerina18@gmail.com>
2022-01-19 10:02:23 -0600

    class today!

---
+++ equationthingy.py
@@ -0,0 +1,29 @@
+import os
+from turtle import clear
+from unittest import result
+os.system('cls')
+
+# michelle akins
+# 1/19/22
+# game design e block
+
+# goal: other operators +-*/ ** exponent % modeules format printing
+
+#define variable
+v1= 10
+v2= 2
+v3=3
+v4=5
+
+#input equation
+answer = (3*v1 - 2*(v2)**2/v3) /v4
+
+#display equation
+print("v1 = %5d"% v1)
+print("v2 = %5d"% v2)
+print("v3 = %5.8f"% v3)
+print("v4 = %5d"% v4)
+
+print("answer = %6.2f"% answer,end="----" )
+
+print("\n The \" quotes \" tabs \t backslash\\")
