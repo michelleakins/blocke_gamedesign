def397955fbfd451884e69de02d9186de91be9d8
michelleakins <michellerina18@gmail.com>
2022-01-21 08:54:10 -0600

    update thingys

---
+++ guessanumbermenu.py
@@ -0,0 +1,20 @@
+import os
+
+print("Guess a number menu")
+
+correct_choice = 89
+
+count = 0
+
+guess = int(input("Guess a number:- "))
+
+if x == correct_choice:
+    print("yay, you got it right")
+
+if x > correct_choice:
+    print("the number is too big")
+    
+if x < correct_choice:
+    print("sorry, that number is too small, guess again")
+    
+    
