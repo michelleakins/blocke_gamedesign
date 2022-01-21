931ee86ebb125175c56aaad2fe35303fe750cf84
michelleakins <michellerina18@gmail.com>
2022-01-21 10:49:24 -0600

    guessing game done!!

--- guessanumbermenu.py
+++ guessanumbermenu.py
@@ -1,22 +1,35 @@
-import os
+import numbers
+import os, random
+os.system('cls')
 
-print("Guess a number menu")
+# michelle akins
+# game design block e
+# 1/21/21
 
-correct_choice = 89
+# we are going to learn about input() function,
+# type casting, branching, looping
 
-count = 0
+#  declare a variable
+print("Enter a number from 1-10:")
+#userInfo= int(input()) #input returns string, we must type cast bc we need a number
+#print("The number is %.2f " %(userInfo/3)) #we use .2 float to get the thingy
 
-x = 1-100
+#guess = int(input("Please give me a number"))
 
-guess = int(input("Guess a number:- "))
+correct_number = random.randint(1,10)
+# print("gcfvhjkloiujghfdghjklo;kljihgfdfghjkl;")
+# print("blahshbhdw nem")
 
-if x == correct_choice:
-    print("yay, you got it right")
+GameOn = True
+while(GameOn):
+    userGuess = int(input("Guess a number from 1-10"))
+    if correct_number == userGuess:
+        print("Yay!! That is correct!!")
+        
+    if userGuess > correct_number:
+        print("Sorry, the number is too big, guess again")
 
-if x > correct_choice:
-    print("the number is too big")
-    
-if x < correct_choice:
-    print("sorry, that number is too small, guess again")
+    if userGuess < correct_number:
+        print("Sorry, that number is too small, guess again")
 
-    
+
