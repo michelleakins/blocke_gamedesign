2b9b51afbe9cdb5f8c69bd0ce0b95699660a5682
michelleakins <michellerina18@gmail.com>
2022-02-01 10:28:41 -0600

    strings!

---
+++ strings.py
@@ -0,0 +1,25 @@
+import os
+os.system('cls')
+#michelle akins
+# 1/31/22
+# strings are an array of characters
+# has many functions!
+
+
+#if u put 3 quotation marks, it prints exactly how it is
+myname = "michelle akins"
+mystatement = """ yayayayayay 
+
+
+
+jihfbhdjnkjehb"""
+print(myname[3])
+print(myname[1])
+print(mystatement)
+print("my last name begins with " +myname[9])
+
+if 'yay' in mystatement:
+    print('true')
+if 'michelle' in myname:
+    print('RA')
+
