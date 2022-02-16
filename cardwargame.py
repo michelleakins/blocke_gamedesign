3f3236edcca6ee211a0fbb9ae198fbc191aac72b
michelleakins <michellerina18@gmail.com>
2022-02-16 10:17:26 -0600

    games

---
+++ cardwargame.py
@@ -0,0 +1,23 @@
+#card war game
+
+def menu():
+    print("menu")
+numbercards= []
+
+for i in range(2,11):
+    numbercards.append(i)
+    numbercards[i-2]= str(numbercards[i-2])
+print(numbercards)
+suits = ['s','d','g','d']
+royals = ['J','K', "Q", "A"]
+
+#make a list from 2 to ace
+# make a deck of cards adding each suite
+
+cards= ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+
+
+
+
+
+
