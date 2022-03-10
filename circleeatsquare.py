57eddb12f3a25e2c690260d63ec7f65d0155c3ed
michelleakins <michellerina18@gmail.com>
2022-03-10 09:11:40 -0600

    circle eats square

--- circleeatsquare.py
+++ circleeatsquare.py
@@ -55,16 +55,18 @@ while check:
     if keys[pygame.K_s] and square.y < HEIGHT - hbox:
         square.y += move
     #finished circle
-    if keys[pygame.K_LEFT] and xc >=radius:
+    if keys[pygame.K_LEFT] and xc >=radius + move:
            xc -= move
-    if keys[pygame.K_RIGHT] and xc < WIDTH - wbox:
+    if keys[pygame.K_RIGHT] and xc < WIDTH - (radius +move):
            xc += move
-    if keys[pygame.K_UP] and yc >=radius:
+    if keys[pygame.K_UP] and yc >=radius + move:
            yc -= move
-    if keys[pygame.K_DOWN] and yc < HEIGHT - hbox:
+    if keys[pygame.K_DOWN] and yc < HEIGHT - (radius + move):
            yc += move
 
     pygame.draw.rect(screen, sq_color, square)
     pygame.draw.circle(screen, cr_color, (xc, yc), radius)
     pygame.display.update()
     pygame.time.delay(10)
+
+    #  if xc and yc is equal to the position of the square, then end the game with "CIRCLE ATE SQUARE"
