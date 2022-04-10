59031d7aa28cdaf60d1263dd76ccecb5a0a88dae
michelleakins <michellerina18@gmail.com>
2022-04-10 01:01:56 -0500

    ben

--- in progress/movingchart.py
+++ in progress/movingchart.py
@@ -1,15 +1,8 @@
 import mailbox
 import os, time, random,math
+from shutil import move
 import pygame as p
-# K_UP                  up circle
-# K_DOWN                down circle
-# K_RIGHT               right circle
-# K_LEFT                left circle
-# K_a                   left square
-# K_d                   right square
-# K_w                   up square
-# K_s                   down square
-# K_SPACE
+
 #initialize pygame
 p.init()
 #variables, constants, and objects
@@ -21,23 +14,33 @@ xc=random.randint(50, WIDTH-50)
 yc=random.randint(50,HEIGHT-50)
 hbox=20  #height if rect
 wbox=20  #width of rect
-#inscribe square
+#inscribe char
 rad = 15
 ibox=rad*math.sqrt(2)
 xi= xc-ibox/2
 yi= yc-ibox/2
 inscSq=p.Rect(xi,yi, ibox,ibox)
+
+walkRight = [p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\R1.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\R2.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\R3.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\R4.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\R5.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\R6.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\R7.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\R8.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\R9.png')]
+walkLeft = [p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\L1.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\L2.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\L3.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\L4.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\L5.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\L6.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\L7.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\L8.png'), p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\L9.png')]
+char = p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\standing.png')
 bg=p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\\bacroung image.jpg')
-man=p.image.load('classstuffyes\images\Pygame-Tutorials-master\Game\L3.png')
 
-manX=0
-manY=HEIGHT-64
-check=True
-manSquare=p.Rect(manX,manY,64,64)
-boulder = p.Rect(230,270,180,345) 
-square=p.Rect(x,y,wbox,hbox)
 
+lock = p.time.Clock()
+
+x = 50
+y = 400
+width = 64
+height = 64
+move = 5
+isJump = False
+jumpCount = 10
+left = False
+right = False
+walkCount = 0
 JUMP=False
+MAX=10
 #create screen
 screen=p.display.set_mode((WIDTH,HEIGHT))
 
@@ -60,96 +63,84 @@ def changeClr():
             randColor=random.choice(list(colors))
         else:
             colorCheck=False
-changeClr()
 sq_color=colors.get(randColor)    
 
 p.display.set_caption("Circle eats Square")
 jumpCount=7
 COUNT=7
-impact=False
-while check:
-    move=10
-    crash=boulder.colliderect(manSquare)
-    if crash:
-        impact = True
-    else:
-        impact=False
 
-    for case in p.event.get():
-        if case.type == p.QUIT:
-            check=False
-    keys=p.key.get_pressed() # this is a list
-    print(impact)
-    if case.type==p.MOUSEBUTTONDOWN:
-        mouse_pos=p.mouse.get_pos()
-        print(mouse_pos)
-    #Square Moves
-    if keys[p.K_a] and manX>=move:
-        square.x -= move
-        # if not impact:
-        manX -= move
-        manSquare.x -= move
-    if keys[p.K_d] and manX<WIDTH-wbox:
-        square.x += move
-        if not impact:
-            manX += move
-            manSquare.x += move
-    #JUMP CODE
-    if not JUMP:
-        if keys[p.K_s]:
-            square.y += move
-            if not impact:
-                manY += move
-                manSquare.y += move
-        if keys[p.K_w]:
-            square.y -= move
-            if not impact:
-                manY -= move
-                manSquare.y -= move
-        if keys[p.K_SPACE]:
-            JUMP=True
-    else:
-        if jumpCount >= -COUNT:
-            square.y -= jumpCount*abs(jumpCount)/2
-            manY -= jumpCount*abs(jumpCount)/2
-            manSquare.y -= jumpCount*abs(jumpCount)/2
-            jumpCount -=1
+keys=p.key.get_pressed() #<-- To check if a key gets pressed (classified as a list), the 'and move' part has to do with creating boundries
+RandColor=random.choice(list(colors))
+#Call colors to get colors for our screen and shapes
+background=colors.get('black')
+# s_color=colors.get('navy') <--- Previous square color
+c_color=colors.get('white')
+Hit_color=colors.get('purple')
+# Creating a color check to make sure our colors are all different:
+changeClr()
+s_color=colors.get(RandColor) 
+
+#square variables
+xs=20
+ys=20
+wbox=30
+hbox=30
+
+#VARIABLES FOR COLLIDE, CIRCLE, AND MOVE
+HitX=xc-15
+HitY=yc-15
+CRadius=15
+HitLenght=CRadius*2
+HitWidth=CRadius*2
+hitbox=pygame.Rect(HitX,HitY,HitWidth,HitLenght)
+move=5
+ColorCheck=False
+
+def playgameyuh():
+    #make a function for our game
+    check = True
+    while check:
+        #Fill the screen and draw the shapes (for testing)
+        screen.fill(background)
+        keys=p.key.get_pressed() #<-- To check if a key gets pressed (classified as a list), the 'and move' part has to do with creating boundries
+        # Movements for the square
+        if keys[p.K_a] and char.x>=move:
+            square.x-=move #subtract
+        if keys[p.K_d] and char.x<=WIDTH-(wbox+move):
+            square.x+=move
+        #Jumping
+        if not JUMP:
+            if keys[p.K_w] and char.y>=move:
+                char.y-=move
+            if keys[p.K_s] and char.y<=HEIGHT-(hbox+move):
+                char.y+=move
+            if keys[p.K_SPACE]:
+                JUMP=True
         else:
-            jumpCount=COUNT
-            JUMP=False
-    
-    #circle moves
-    if keys[p.K_LEFT] and xc > move:
-        xc -= move
-        inscSq.x -= move
-    if keys[p.K_RIGHT] and xc <WIDTH:
-        xc += move
-        inscSq.x += move
-    if keys[p.K_DOWN] and yc <HEIGHT:
-        yc += move
-        inscSq.y += move
-    if keys[p.K_UP] and yc > move:
-        yc -= move
-        inscSq.y -= move
-
-   # choque=square.collidepoint((xc,yc))
-    choque=square.colliderect(inscSq)
-    if choque:
-        square.x=random.randint(50, WIDTH-50)
-        square.y=random.randint(50,HEIGHT-50)
-        changeClr()
-        sq_color=colors.get(randColor)    
-        xi= xc-ibox/2
-        yi= yc-ibox/2
-        inscSq=p.Rect(xi,yi, ibox,ibox)
-    #Check clide between the man and the bulder
-    
-    
-
-    p.draw.rect(screen,1,manSquare)
-    p.draw.rect(screen,blColor, boulder)
-    screen.blit(bg,(0,0))
-    screen.blit(man,(manX,manY))
-    p.draw.rect(screen,sq_color,inscSq)
-    p.display.update()
-    p.time.delay(30)
+            if jumpCount>=-MAX:
+                char.y-=jumpCount*abs(jumpCount)/2
+                jumpCount-=1
+            else:
+                jumpCount=MAX
+                JUMP=False
+        # Circle Movements
+        if keys[p.K_LEFT] and xc>=move+CRadius:
+            xc-=move #subtract
+            walkCount.x-=move
+        if keys[p.K_RIGHT] and xc<=WIDTH-(CRadius+move):
+            xc+=move
+            walkRight.x+=move
+        # if keys[p.K_UP] and yc>=move+CRadius:
+        #     yc-=move
+        #     hitbox.y-=move
+        # if keys[p.K_DOWN] and yc<=HEIGHT-(CRadius+move):
+        #     yc+=move
+        #     hitbox.y+=move
+        #Making the collision
+        checkCollide= char.colliderect(hitbox)
+        if checkCollide==True:
+            char.x=random.randint(wbox,WIDTH-wbox)
+            char.y=random.randint(hbox,HEIGHT-hbox)
+            CRadius+=move
+            changeClr()
+            ColorCheck=True
